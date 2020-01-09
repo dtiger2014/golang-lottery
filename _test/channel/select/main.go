2391d69829aa86d61f0aa614076592f844618b8f
fanfan <piaolongfan@xiimoon.com>
2020-01-09 10:34:44 +0800

    update

--- _test/channel/select/main.go
+++ _test/channel/select/main.go
@@ -1,9 +1,9 @@
 package main
 
-import(
+import (
 	"fmt"
-	"time"
 	"math/rand"
+	"time"
 )
 
 func generator() chan int {
@@ -12,26 +12,58 @@ func generator() chan int {
 	go func() {
 		i := 0
 		for {
-			time.Sleep(time.Duration(rand.Intn(1500)) * time.Millisecond)
+			time.Sleep(2 * time.Duration(rand.Intn(1500)) * time.Millisecond)
 			out <- i
 			i++
-		} 
+		}
 	}()
 
 	return out
 }
 
+func worker(id int, c chan int) {
+	for n := range c {
+		time.Sleep(time.Second)
+		fmt.Printf("Worker %d received %d\n", id, n)
+	}
+}
+
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+	return c
+}
+
 func main() {
 	var c1, c2 chan int = generator(), generator()
+	var worker = createWorker(0)
 
+	var values []int
+	tm := time.After(10 * time.Second)
+	tick := time.Tick(time.Second)
 	for {
+		var activeWorker chan<- int
+		var activeValue int
+		if len(values) > 0 {
+			activeWorker = worker
+			activeValue = values[0]
+		}
+
 		select {
 		case n := <-c1:
-			fmt.Println("Reveived from c1:",n)
+			values = append(values, n)
 		case n := <-c2:
-			fmt.Println("Reveived from c2:",n)
-		// default:
-		// 	fmt.Println("No value received")
+			values = append(values, n)
+		case activeWorker <- activeValue:
+			values = values[1:]
+
+		case <-time.After(800 * time.Millisecond):
+			fmt.Println("timeout")
+		case <-tick:
+			fmt.Println("queue len = ", len(values))
+		case <-tm:
+			fmt.Println("bye")
+			return
 		}
 	}
 }
